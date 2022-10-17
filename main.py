eaff4ea1525ec6b462f4f80c99865cb95d11ba24
Pedro <pedroblimaa@gmail.com>
2022-10-17 18:00:16 -0300

    Organize project, and get characters name and image

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import scrap_akuma_no_mi
-import json_manager
+import scrap.scrap_akuma_no_mi as scrap_akuma_no_mi
+import scrap.scrap_characters as scrap_characters
+import utils.json_manager as json_manager
 
 URL = 'https://onepiece.fandom.com/wiki'
 
 
 if __name__ == '__main__':
-    data = scrap_akuma_no_mi.get_all()
-    json_manager.save_to_json(data, 'akuma_no_mi.json')
+    data = scrap_characters.get_all()
+    json_manager.save_to_json(data, 'characters.json')
